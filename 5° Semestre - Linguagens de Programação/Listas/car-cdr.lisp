f90d1ac1d0b1198b7a28d0133272cac4a202cc65
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2025-03-26 20:11:40 -0300

    Create car-cdr.lisp

---
+++ 5° Semestre - Linguagens de Programação/Listas/car-cdr.lisp
@@ -0,0 +1,2 @@
+(print (car '(o poderoso chefao))) ;; CAR é um ponteiro que aponta para o primeiro elemento da lista
+(print (cdr '(o poderoso chefao))) ;; CDR é um ponteiro que aponta para o restante da lista
